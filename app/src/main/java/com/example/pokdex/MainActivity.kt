e5ecd0c9471f36331c877a9ce4c7e51b9f58ef46
Domininiuk <allorify@gmail.com>
2021-10-14 19:26:55 +0200

    Added empty toolbar

--- app/src/main/java/com/example/pokdex/MainActivity.kt
+++ app/src/main/java/com/example/pokdex/MainActivity.kt
@@ -4,8 +4,13 @@ package com.example.pokdex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity()
 {
@@ -20,10 +25,16 @@ class MainActivity : AppCompatActivity()
 
        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
        val navController = navHostFragment!!.findNavController()
-
-
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+     //       setupWithNavController(toolbar, navController)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
