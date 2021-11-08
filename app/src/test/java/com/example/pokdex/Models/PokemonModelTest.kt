d93fd488899f8f415b6951589e39aabd1b8df112
Domininiuk <allorify@gmail.com>
2021-11-08 21:49:57 +0100

    Starting testing models

---
+++ app/src/test/java/com/example/pokdex/Models/PokemonModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.pokdex.Models
+
+import junit.framework.TestCase
+
+class PokemonModelTest : TestCase() {
+
+    fun testGetOfficialArtworkFrontDefault() {}
+
+    fun testGetHeightInCentimeters() {}
+
+    fun testGetWeightInKilograms() {}
+}
